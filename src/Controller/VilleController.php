50580b12fc8cf1d9243b838e8b5fcd3d7270a4f0
Alt Ersine <alexiasanchez@live.fr>
2026-03-06 14:07:06 +0100

    new feature : En tant qu’administrateur, je peux ajouter des villes utilisables dans la plateforme.

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Ville;
+use App\Form\VilleType;
 use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/ville', name: 'app_ville')]
 final class VilleController extends AbstractController
 {
-    #[Route('/ville/cp', name: 'app_ville_cp')]
+    #[Route('/cp', name: '_cp')]
     public function getCp(
         Request $request,
         VilleRepository $villeRepository,
@@ -21,6 +27,26 @@ final class VilleController extends AbstractController
 
         return $this->json([
             'codePostal' => $ville?->getCodePostal(),
+        ]);
+    }
+
+    #[Route("/create", name: '_create')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function create(Request $request, EntityManagerInterface $em): Response {
+        $newVille = new Ville();
+        $form = $this->createForm(VilleType::class, $newVille);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($newVille);
+            $em->flush();
+
+            $this->addFlash('success','Votre ville a été ajoutée à la liste');
+            return $this->redirectToRoute('sortie_liste');
+        }
+
+        return $this->render('ville/create.html.twig', [
+            'form' => $form
         ]);
     }
 }
---
+++ src/Form/VilleType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VilleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'name',
+                'help' => '/!\ le choix du lieu d\'une sortie est filtré sur le campus.
+                Créez une ville qui fasse partie de la même zone géographique que ce choix.',
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la ville'
+            ])
+            ->add('codePostal', TextType::class, [
+                'label' => 'Code postal'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-success',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ville::class,
+        ]);
+    }
+}
